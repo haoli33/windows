e06cc47431a80a7ea1c4d0973d0304251c6af1f3
田昊丽 <55847941+haoli33@users.noreply.github.com>
2020-11-09 18:39:52 +0800

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "Lib1.h"
+using namespace std;
+#pragma comment(lib,"../Debug/Lib1.lib")
+int main()
+{
+	int a = 30;
+	int b = 12;
+	cout << SumSquare(a, b) << endl;
+	cout << SquareSum(a, b) << endl;
+
+	system("pause");
+	return 0;
+
+}
+
